5ee57df2020d4c6752822e76c8a02bdd43a553a2
JialinWu <win27v@gmail.com>
2017-08-23 12:24:05 +0800

    as one func

--- main.go
+++ main.go
@@ -1,16 +1,50 @@
 package main
 
-import "github.com/linnv/logx"
+import (
+	"flag"
+	"os"
+	"strings"
+)
 
 var (
 	Version   = "1.0.0"
-	BuildTime string
-	Demo      string
+	BuildTime = "2017-08-23_11:44:38"
 )
 
-//go build -ldflags "-X 'main.Version=`git rev-parse HEAD`' -X 'main.BuildTime=`date  '+%Y-%m-%d_%H:%M:%S'`' -X main.Demo=3" main.go
+//go build -ldflags "-X 'main.Version=`git rev-parse HEAD`' -X 'main.BuildTime=`date  '+%Y-%m-%d_%H:%M:%S'`' " main.go
+
+func VersionGuide() {
+	if !flag.Parsed() {
+		os.Stderr.Write([]byte("ERROR: do flag.Parse() first!"))
+		return
+	}
+
+	args := flag.Args()
+	versionList := [...]string{
+		"V", "-V", "--V",
+		"VERSION", "-VERSION", "--VERSION",
+	}
+	buildTimeList := [...]string{
+		"T", "-T", "--T",
+		"BUILDTIME", "-BUILDTIME", "--BUILDTIME",
+	}
+	for _, v := range args {
+		upperV := strings.ToUpper(v)
+		for _, ver := range versionList {
+			if ver == upperV {
+				os.Stdout.Write([]byte("Version: " + Version + "\n"))
+			}
+		}
+		for _, ver := range buildTimeList {
+			if ver == upperV {
+				os.Stdout.Write([]byte("BuildTime: " + BuildTime + "\n"))
+			}
+		}
+	}
+}
+
 func main() {
-	logx.Debugf("Version: %+v\n", Version)
-	logx.Debugf("BuildTime: %+v\n", BuildTime)
-	logx.Debugf("Demo: %+v\n", Demo)
+	flag.Parse()
+
+	VersionGuide()
 }
