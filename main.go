cc7f93c24051033326cf8b20510d762c8c652908
JialinWu <win27v@gmail.com>
2017-08-23 11:07:09 +0800

    delete wrong formatting flag cmd

--- main.go
+++ main.go
@@ -8,16 +8,6 @@ var (
 	Demo      string
 )
 
-//go build -ldflags "-X main.BuildTime `date -u '+%Y-%m-%d_%I:%M:%S%p'` -X main.Version `git rev-parse HEAD`"
-// go.18 or higher
-//go build -ldflags "-s -X /Users/Jialin/golang/src/version_demo.BuildTime=`date -u '+%Y-%m-%d_%I:%M:%S%p'` -X /Users/Jialin/golang/src/version_demo.Version=`git rev-parse HEAD`" main.go
-
-//go build -ldflags "-X /Users/Jialin/golang/src/version_demo/main.BuildTime=`date -u '+%Y-%m-%d_%I:%M:%S%p'`" main.go
-
-//go build -ldflags "-X version_demo/main.BuildTime=`date -u '+%Y-%m-%d_%I:%M:%S%p'`" main.go
-
-//go build -ldflags "-X version_demo/main.BuildTime=11 " main.go
-
 //go build -ldflags "-X 'main.Version=`git rev-parse HEAD`' -X 'main.BuildTime=`date  '+%Y-%m-%d_%H:%M:%S'`' -X main.Demo=3" main.go
 func main() {
 	logx.Debugf("Version: %+v\n", Version)
