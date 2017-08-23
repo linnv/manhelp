c7721431c59af16809020923e776ea9089071901
JialinWu <win27v@gmail.com>
2017-08-23 10:29:19 +0800

    version flag

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import "github.com/linnv/logx"
+
+var (
+	Version   = "1.0.0"
+	BuildTime = "2015-08-01 UTC"
+)
+
+//go build -ldflags "-X main.BuildTime `date -u '+%Y-%m-%d_%I:%M:%S%p'` -X main.Version `git rev-parse HEAD`"
+//go build -ldflags "-X /Users/Jialin/golang/src/version_demo.BuildTime=`date -u '+%Y-%m-%d_%I:%M:%S%p'` -X /Users/Jialin/golang/src/version_demo.Version=`git rev-parse HEAD`" main.go
+func main() {
+	logx.Debugf("Version: %+v\n", Version)
+	logx.Debugf("BuildTime: %+v\n", BuildTime)
+}
